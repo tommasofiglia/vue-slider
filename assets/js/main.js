ef5633b959c96be6249cf049847d634140ff9e7f
Tommaso Figlia <tommasofiglia@gmail.com>
2020-12-15 16:29:50 +0100

    Immagine successiva al click aggiunta

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,20 @@
-let app = new Vue({
+let root = new Vue({
   // options object
-  el: "#app",
+  el: "#root",
+  data: {
+    images: [
+      "https://images.pexels.com/photos/371633/pexels-photo-371633.jpeg?cs=srgb&dl=clouds-country-daylight-371633.jpg&fm=jpg",
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-646511634.jpg",
+      "https://cdn.mos.cms.futurecdn.net/FUE7XiFApEqWZQ85wYcAfM.jpg",
+      "https://static.photocdn.pt/images/articles/2017/04/28/iStock-546424192.jpg"
+    ],
+    posizione: 0
+  },
+  methods: {
+    // Funzione per il click sul next
+      goAhead() {
+        this.posizione++
+      }
+  }
+
 });
